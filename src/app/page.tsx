ccd29250d0527dfcbdb4c663b150d8729d4af847
louanbel <louanbelicaud63@gmail.com>
2025-04-27 18:40:19 +0200

    simple home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
+"use client";
+
 import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
 import Login from "~/Login";
 
-interface AppProps {
-  pageProps?: {
-    session?: Session | null;
-    [key: string]: any;
-  };
-}
-
-export default function App({ pageProps = {} }: AppProps) {
-  const { session, ...restPageProps } = pageProps;
-
+export default function Home() {
   return (
-    <SessionProvider session={session}>
-      <Login {...restPageProps} />
+    <SessionProvider>
+      <Login />
     </SessionProvider>
   );
 }
